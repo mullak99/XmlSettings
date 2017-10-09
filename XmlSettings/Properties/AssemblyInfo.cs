1b6dd11fa18f7e57d6da33ae59d43b44cce3bd96
mullak99 <mullak99@mullak99.co.uk>
2017-10-09 20:13:09 +0100

    Add project files.

---
+++ XmlSettings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("XmlSettings")]
+[assembly: AssemblyDescription("A simple library for reading and writing program settings to an XML file.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("mullak99")]
+[assembly: AssemblyProduct("XmlSettings")]
+[assembly: AssemblyCopyright("GPL 3.0 | mullak99 - 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("111e6d07-a928-4682-8663-dd35a0de5245")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ XmlSettings/XmlSettings.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Xml;
+
+public class XmlSettings
+{
+    private string mainSettingsPath;
+    private bool revertOnFail;
+    private const string xmlFormatVer = "1.0";
+
+    XmlDocument doc = new XmlDocument();
+    XmlDocument loadedDoc = new XmlDocument();
+    XmlElement xmlBody;
+    string xmlContents;
+
+    /// <summary>
+    /// Allows the use of the XmlSettings library.
+    /// </summary>
+    /// <param name="settingsPath">The path for the settings file, can either be relative or absolute.</param>
+    /// <param name="revertToDefaultOnFail">States if the settings should be reverted to the default if it cannot be parsed.</param>
+    public XmlSettings(string settingsPath, bool revertToDefaultOnFail = true)
+    {
+        mainSettingsPath = settingsPath;
+        revertOnFail = revertToDefaultOnFail;
+
+        if (!File.Exists(mainSettingsPath))
+        {
+            XmlDeclaration xmlDeclaration = doc.CreateXmlDeclaration(xmlFormatVer, "UTF-8", null);
+            XmlElement root = doc.DocumentElement;
+            doc.InsertBefore(xmlDeclaration, root);
+
+            xmlBody = doc.CreateElement(string.Empty, "body", string.Empty);
+            doc.AppendChild(xmlBody);
+
+            saveXML();
+        }
+    }
+
+    /// <summary>
+    /// Used to get the current version of XmlSettings.
+    /// </summary>
+    /// <returns>The version number of XmlSettings.</returns>
+    public string getVersion()
+    {
+        return FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).ProductVersion;
+    }
+
+    /// <summary>
+    /// Used to get the max supported formatting version of the Xml Document that can be read by this version of XmlSettings.
+    /// </summary>
+    /// <returns>The latest supported formatting version that XmlSettings can work with.</returns>
+    public string getLatestSupportedFormattingVersion()
+    {
+        return xmlFormatVer;
+    }
+
+    //Creates and sets the default value for various datatypes
+    public bool addBoolean(string varName, bool defaultValue)
+    {
+        if (!doesDuplicateExist(varName))
+        {
+            XmlNode booleanNode = doc.CreateElement("boolean");
+            xmlBody.AppendChild(booleanNode);
+
+            XmlNode varNode = doc.CreateElement(varName);
+            XmlAttribute defaultVal = doc.CreateAttribute("default");
+            defaultVal.Value = defaultValue.ToString();
+            varNode.Attributes.Append(defaultVal);
+            varNode.InnerText = defaultValue.ToString();
+            booleanNode.AppendChild(varNode);
+
+            saveXML();
+
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool addString(string varName, string defaultValue)
+    {
+        if (!doesDuplicateExist(varName))
+        {
+            XmlNode stringNode = doc.CreateElement("string");
+            xmlBody.AppendChild(stringNode);
+
+            XmlNode varNode = doc.CreateElement(varName);
+            XmlAttribute defaultVal = doc.CreateAttribute("default");
+            defaultVal.Value = defaultValue;
+            varNode.Attributes.Append(defaultVal);
+            varNode.InnerText = defaultValue;
+            stringNode.AppendChild(varNode);
+
+            saveXML();
+
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool addLong(string varName, long defaultValue)
+    {
+        if (!doesDuplicateExist(varName))
+        {
+            XmlNode longNode = doc.CreateElement("long");
+            xmlBody.AppendChild(longNode);
+
+            XmlNode varNode = doc.CreateElement(varName);
+            XmlAttribute defaultVal = doc.CreateAttribute("default");
+            defaultVal.Value = defaultValue.ToString();
+            varNode.Attributes.Append(defaultVal);
+            varNode.InnerText = defaultValue.ToString();
+            longNode.AppendChild(varNode);
+
+            saveXML();
+
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool addInt(string varName, int defaultValue)
+    {
+        if (!doesDuplicateExist(varName))
+        {
+            XmlNode intNode = doc.CreateElement("int");
+            xmlBody.AppendChild(intNode);
+
+            XmlNode varNode = doc.CreateElement(varName);
+            XmlAttribute defaultVal = doc.CreateAttribute("default");
+            defaultVal.Value = defaultValue.ToString();
+            varNode.Attributes.Append(defaultVal);
+            varNode.InnerText = defaultValue.ToString();
+            intNode.AppendChild(varNode);
+
+            saveXML();
+
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool addDouble(string varName, double defaultValue)
+    {
+        if (!doesDuplicateExist(varName))
+        {
+            XmlNode doubleNode = doc.CreateElement("double");
+            xmlBody.AppendChild(doubleNode);
+
+            XmlNode varNode = doc.CreateElement(varName);
+            XmlAttribute defaultVal = doc.CreateAttribute("default");
+            defaultVal.Value = defaultValue.ToString();
+            varNode.Attributes.Append(defaultVal);
+            varNode.InnerText = defaultValue.ToString();
+            doubleNode.AppendChild(varNode);
+
+            saveXML();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    //Sets the value for various datatypes
+    public bool setBoolean(string varName, bool state)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count == 1)
+        {
+            elemList[0].InnerText = state.ToString();
+            forceSaveOnLoadedXML();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool setString(string varName, string state)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count == 1)
+        {
+            elemList[0].InnerText = state;
+            forceSaveOnLoadedXML();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool setInt(string varName, int state)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count == 1)
+        {
+            elemList[0].InnerText = state.ToString();
+            forceSaveOnLoadedXML();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool setLong(string varName, long state)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count == 1)
+        {
+            elemList[0].InnerText = state.ToString();
+            forceSaveOnLoadedXML();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool setDouble(string varName, double state)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count == 1)
+        {
+            elemList[0].InnerText = state.ToString();
+            forceSaveOnLoadedXML();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    //Reads the current value for various datatypes
+
+    public bool readBoolean(string varName)
+    {
+        try
+        {
+            loadXML();
+            XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+            if (elemList.Count > 0)
+                return bool.Parse(elemList[0].InnerXml);
+            else
+                return false;
+        }
+        catch
+        {
+            if (revertOnFail) revertToDefault(varName);
+            return false;
+        }
+    }
+
+    public string readString(string varName)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count > 0)
+            return elemList[0].InnerXml.ToString();
+        else
+            return "";
+    }
+
+    public int readInt(string varName)
+    {
+        try
+        {
+            loadXML();
+            XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+            if (elemList.Count > 0)
+                return int.Parse(elemList[0].InnerXml);
+            else
+                return 0;
+        }
+        catch
+        {
+            if (revertOnFail) revertToDefault(varName);
+            return 0;
+        }
+
+    }
+
+    public long readLong(string varName)
+    {
+        try
+        {
+            loadXML();
+            XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+            if (elemList.Count > 0)
+                return long.Parse(elemList[0].InnerXml);
+            else
+                return 0;
+        }
+        catch
+        {
+            if (revertOnFail) revertToDefault(varName);
+            return 0;
+        }
+    }
+
+    public double readDouble(string varName)
+    {
+        try
+        {
+            loadXML();
+            XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+            if (elemList.Count > 0)
+                return double.Parse(elemList[0].InnerXml);
+            else
+                return 0;
+        }
+        catch
+        {
+            if (revertOnFail) revertToDefault(varName);
+            return 0;
+        }
+    }
+
+    public bool revertToDefault(string varName)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count == 1)
+        {
+            elemList[0].InnerText = elemList[0].Attributes["default"].Value;
+            forceSaveOnLoadedXML();
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool doesVariableExist(string varName)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count > 0)
+            return true;
+        else
+            return false;
+    }
+
+    public string getVarType(string varName)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count > 0)
+            return uppercaseFirst(elemList[0].ParentNode.Name);
+        else
+            return null;
+    }
+
+    private void saveXML()
+    {
+        doc.Save(mainSettingsPath);
+    }
+
+    private void forceSaveOnLoadedXML()
+    {
+        loadedDoc.Save(mainSettingsPath);
+    }
+
+    private void loadXML()
+    {
+        loadedDoc.Load(mainSettingsPath);
+        xmlContents = loadedDoc.InnerXml;
+    }
+
+    private bool doesDuplicateExist(string varName)
+    {
+        loadXML();
+        XmlNodeList elemList = loadedDoc.GetElementsByTagName(varName);
+        if (elemList.Count > 0)
+            return true;
+        else
+            return false;
+    }
+
+    private string uppercaseFirst(string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            return string.Empty;
+
+        return char.ToUpper(s[0]) + s.Substring(1);
+    }
+}
